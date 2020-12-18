df9e4b571abeaf08f0a6ab34b87f1bf45f3026c4
Arun_vsp <Arun_vsp@Arunvsp>
2020-12-18 16:17:34 +0530

    created project with classes

---
+++ DemoApp/src/com/lti/model/Calc.java
@@ -0,0 +1,13 @@
+package com.lti.model;
+
+public class Calc {
+
+	void add(int num1,int num2)
+	{
+		System.out.println("sum="+(num1+num2));
+	}
+	void sub(int num1,int num2)
+	{
+		System.out.println("sub="+(num1-num2));
+	}
+}
---
+++ DemoApp/src/com/lti/model/sample.java
@@ -0,0 +1,8 @@
+package com.lti.model;
+
+public class sample {
+ void print()
+ {
+	 System.out.println("hello everyone");
+ }
+}
