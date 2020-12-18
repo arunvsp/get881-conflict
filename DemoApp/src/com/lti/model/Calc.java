fcff440cde8ea743bf295d90e180c7e35fc5836b
Arun_vsp <Arun_vsp@Arunvsp>
2020-12-18 16:28:34 +0530

    modified add method

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,7 +4,7 @@ public class Calc {
 
 	void add(int num1,int num2)
 	{
-		System.out.println("sum="+(num1+num2));
+		System.out.println("summation"+(num1+num2));
 	}
 	void sub(int num1,int num2)
 	{
